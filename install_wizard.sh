80274ed91548380a5168ef84d4054b1e06cd3164
Ivan Pidikseev <svetlyi62@gmail.com>
2019-03-03 02:01:23 +0300

    added utis

--- install_wizard.sh
+++ install_wizard.sh
@@ -8,6 +8,7 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
   cp .bash_profile ~/.bash_profile
   . ~/.bash_profile
+  sudo chmod 740 ~/.bash_profile
 else
   echo "skipping"
 fi
@@ -19,6 +20,7 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
   cp .bash_aliases ~/.bash_aliases
   . ~/.bash_aliases
+  sudo chmod 740 ~/.bash_aliases
 else
   echo "skipping"
 fi
@@ -30,6 +32,18 @@ read -r -p "Installing vim+plugins [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
  . install_vim.sh
+else
+  echo "skipping"
+fi
+
+
+# install utils
+
+read -r -p "Installing utils [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  sudo cp utils/backitup.sh /usr/local/bin/backitup
+  sudo cp utils/syncit.sh /usr/local/bin/syncit
 else
   echo "skipping"
 fi
--- utils/syncit.sh
+++ utils/syncit.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+echo "Maximum size is 1G!!!111"
+
 if [ -d "$1" ]; then
     excludeFile="$1/exclude-list.txt" # exclude files and folders from exclude-list.txt
     if [ -e "$excludeFile" ]; then
         echo "Exclude file exists"
-        rsync -crtkv --max-size=1g --progress --exclude-from $excludeFile --no-links $1 $2
+        rsync -rtkv --max-size=1g --progress --exclude-from $excludeFile --no-links $1 $2
     else
         echo "Exclude file does not exists"
-        rsync -crtkv --max-size=1g --progress --no-links $1 $2
+        rsync -rtkv --max-size=1g --progress --no-links $1 $2
     fi
     echo 'done'
+else
+    echo "Example: syncit from to"
 fi
